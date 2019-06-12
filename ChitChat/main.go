6438540325f5b555bb31097529a125ce4be607c7
death_finger <death_finger@sina.com>
2019-06-12 22:15:51 +0800

    0612 22:15

--- ChitChat/main.go
+++ ChitChat/main.go
@@ -1,9 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	// 打印启动信息
 	fmt.Println("ChitChat", version(), "started at", config.Address)
+
+	// 创建一个多路复用器
+	mux := http.NewServeMux()
+	// 处理静态文件
+	files := http.FileServer(http.Dir(config.Static))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	// 路由处理
+	// index
+	mux.HandleFunc("/", index)
+	// error
+	mux.HandleFunc("/err", err)
 
 }
--- ChitChat/utils.go
+++ ChitChat/utils.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"os"
 )
+
+type Configuration struct {
+	Address      string
+	ReadTimeout  int64
+	WriteTimeout int64
+	Static       string
+}
+
+var config Configuration
+var logger *log.Logger
 
 func version() string {
 	return "0.1"
 }
 
 func loadConfig() {
+	// 打开配置文件
 	file, err := os.Open("config.json")
 	if err != nil {
 		log.Fatalln("Cannot open log file", err)
+	}
 
+	// 读取 JSON
+	decoder := json.NewDecoder(file)
+	config = Configuration{}
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatalln("Cannot parse configuration from file", err)
 	}
 }
